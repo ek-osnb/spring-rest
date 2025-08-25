02c2e56f82878120ebf8c043d4368886963c3bfa
osman-butt <butt.osman@gmail.com>
2025-08-25 10:44:03 +0200

    initial commit

---
+++ src/test/java/ek/osnb/courseapi/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ek.osnb.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
